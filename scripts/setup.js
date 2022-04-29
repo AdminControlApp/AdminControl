08eb8c7f34165f86a6a9d39a782488270bcfbeb6
Leon Si <contact@leonzalion.com>
2022-04-29 10:35:49 -0400

    chore: keep dist/.gitkeep

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,8 +1,9 @@
 import { execSync } from 'node:child_process';
 import * as fs from 'node:fs';
 
-if (!fs.existsSync('packages/phone-call-pass')) {
+if (!fs.existsSync('packages/phone-call-pass/package.json')) {
 	try {
+		fs.rmSync('packages/phone-call-pass', { recursive: true, force: true });
 		execSync(
 			'git clone git@github.com:AdminControlApp/phone-call-pass ./packages/phone-call-pass'
 		);
