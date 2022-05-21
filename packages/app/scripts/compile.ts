8da8f7b2f87e5544444524a2c65c3eadc443056f
Leon Si <contact@leonzalion.com>
2022-05-20 20:36:25 -0400

    fix: npmrc

--- packages/app/scripts/compile.ts
+++ packages/app/scripts/compile.ts
@@ -1,8 +1,6 @@
 import * as execa from '@commonjs/execa';
 import { chProjectDir } from 'lionconfig';
 
-// Create a temporary `dist/` folder to create the final bundled app in
-
 chProjectDir(__dirname);
 
 execa.commandSync('pnpm run build', {
@@ -10,11 +8,7 @@ execa.commandSync('pnpm run build', {
 	env: { MODE: 'production' },
 });
 
-execa.commandSync('pnpm install --shamefully-hoist', { stdio: 'inherit' });
-
 execa.commandSync(
 	'electron-builder build --config .electron-builder.config.js --dir --config.asar=false',
 	{ stdio: 'inherit', env: { MODE: 'production' } }
 );
-
-execa.commandSync('pnpm install', { stdio: 'inherit' });
