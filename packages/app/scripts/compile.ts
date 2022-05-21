5b527aae858963d17a7bbe858e4dcc03e0b4e098
Leon Si <contact@leonzalion.com>
2022-05-21 09:57:00 -0400

    fix: compile script

--- packages/app/scripts/compile.ts
+++ packages/app/scripts/compile.ts
@@ -1,6 +1,6 @@
 import * as execa from '@commonjs/execa';
-import isCi from 'is-ci';
 import { chProjectDir } from 'lionconfig';
+import process from 'node:process';
 
 chProjectDir(__dirname);
 
@@ -9,14 +9,15 @@ execa.commandSync('pnpm run build', {
 	env: { MODE: 'production' },
 });
 
-if (isCi) {
+if (process.env.GITHUB_ACTIONS === undefined) {
 	execa.commandSync(
-		'electron-builder build --config .electron-builder.config.js --dir --config.asar=false --mac --publish always',
+		'electron-builder build --config .electron-builder.config.js --dir --config.asar=false',
 		{ stdio: 'inherit', env: { MODE: 'production' } }
 	);
 } else {
+	console.info('CI detected; building for publish...');
 	execa.commandSync(
-		'electron-builder build --config .electron-builder.config.js --dir --config.asar=false',
+		'electron-builder build --config .electron-builder.config.js --config.asar=false --mac --publish always',
 		{ stdio: 'inherit', env: { MODE: 'production' } }
 	);
 }
