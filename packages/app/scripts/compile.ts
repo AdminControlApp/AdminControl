4cedfab5361b16036e61bd91fd99c4441b474d0b
Leon Si <contact@leonzalion.com>
2022-05-21 09:41:23 -0400

    chore: compile

--- packages/app/scripts/compile.ts
+++ packages/app/scripts/compile.ts
@@ -1,4 +1,5 @@
 import * as execa from '@commonjs/execa';
+import isCi from 'is-ci';
 import { chProjectDir } from 'lionconfig';
 
 chProjectDir(__dirname);
@@ -8,7 +9,14 @@ execa.commandSync('pnpm run build', {
 	env: { MODE: 'production' },
 });
 
-execa.commandSync(
-	'electron-builder build --config .electron-builder.config.js --dir --config.asar=false',
-	{ stdio: 'inherit', env: { MODE: 'production' } }
-);
+if (isCi) {
+	execa.commandSync(
+		'electron-builder build --config .electron-builder.config.js --dir --config.asar=false --mac --publish always',
+		{ stdio: 'inherit', env: { MODE: 'production' } }
+	);
+} else {
+	execa.commandSync(
+		'electron-builder build --config .electron-builder.config.js --dir --config.asar=false',
+		{ stdio: 'inherit', env: { MODE: 'production' } }
+	);
+}
